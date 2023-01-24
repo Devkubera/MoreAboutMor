63f581b4ba0f474a820a68b500ae9947d7f662c2
Gunzy <tokiatpbvip1@gmail.com>
2023-01-24 22:07:29 +0700

    Update

--- app/src/main/java/com/example/moreaboutmoreapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/moreaboutmoreapp/Activities/MainActivity.java
@@ -34,7 +34,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.inappmessaging.FirebaseInAppMessaging;
 import com.google.gson.Gson;
 
 import java.io.BufferedReader;
