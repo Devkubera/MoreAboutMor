202fb7c6b7dd3b1b9e47185c94ee407993174650
Gunzy <tokiatpbvip1@gmail.com>
2023-01-24 22:35:42 +0700

    Add FirebaseInAppMessaging

--- app/src/main/java/com/example/moreaboutmoreapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/moreaboutmoreapp/Activities/MainActivity.java
@@ -34,6 +34,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.inappmessaging.FirebaseInAppMessaging;
 import com.google.gson.Gson;
 
 import java.io.BufferedReader;
